ba58f2cde9d4c171d661e6d8cbe0beaa75295924
woodev01 <woodev01@luther.edu>
2016-04-13 17:32:28 -0500

    Added files via upload

---
+++ Calculator.Project3/Calculator.Project3Tests/Calculator_Project3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Calculator_Project3Tests.swift
+//  Calculator.Project3Tests
+//
+//  Created by Evan on 4/13/16.
+//  Copyright © 2016 Evan. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_Project3
+
+class Calculator_Project3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
